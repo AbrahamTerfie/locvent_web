f64ed7e8c6fa2e02cefe7945ac4d0c3dd0e52953
Abraham Terfie <abrahamterfie4563@gmail.com>
2021-09-15 05:02:31 +0300

    feat: agent tables

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,13 +1,59 @@
-import React from 'react'
+import React from "react";
+import MaterialTable from "material-table";
+
 const Contact = () => {
   return (
-    <div className="container">
-    
-      <h1 className="text-center" style={{paddingTop: "30%"}}>
-        Contactss page 
-      </h1>
-      
+    <div
+      style={{
+        padding: "2em",
+        color: "#095B59",
+        backgroundColor: "#F5F5F5",
+      }}
+    >
+      <h4 className="p-4">All Agents</h4>
+      <MaterialTable
+        style={{
+          color: "#095B59",
+          fontFamily: "Roboto, sans-serif",
+          fontSize: ".8em",
+          border: "1px solid #e5e5e5",
+        }}
+        title="All agents"
+        columns={[
+          { title: "Name", field: "name" },
+          { title: "Surname", field: "surname" },
+          { title: "Birth Year", field: "birthYear", type: "numeric" },
+          { title: "year joined", field: "dateJoined", type: "date" },
+          { title: "is expert", field: "expert", type: "boolean" },
+          {
+            title: "region",
+            field: "region",
+            tooltip: "Click to filter",
+          },
+        ]}
+        data={[
+          {
+            name: "Mehmet",
+            surname: "Baran",
+            birthYear: 1987,
+            dateJoined: new Date().getFullYear(),
+            region: "Bolu",
+            expert: true,
+          },
+          {
+            name: "Zerya Betül",
+            surname: "Baran",
+            birthYear: 2017,
+            dateJoined: new Date().getFullYear(),
+            region: "addis ababa",
+            expert: false,
+          },
+        ]}
+        options={{
+          exportButton: true,
+        }}
+      />
     </div>
-  )
-}
+  );
+};
 export default Contact;
