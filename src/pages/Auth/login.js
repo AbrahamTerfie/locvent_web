5e680bb02611a851d68f353bb274e3844708e0bb
Abraham Terfie <abrahamterfie4563@gmail.com>
2021-09-16 13:56:16 +0300

    feat : login screen ui

--- src/pages/Auth/login.js
+++ src/pages/Auth/login.js
@@ -1,9 +1,91 @@
-import React from 'react'
-
+import React from "react";
+import { Col, Form, FormGroup, Label, Input, Button } from "reactstrap";
 export default function Login() {
-    return (
-        <div>
-            this is login screen 
+  return (
+    <div
+      style={{
+        margin: "20%",
+        height: "20em",
+        width: "60em",
+        border: "2px solid #095B59",
+        borderRadius: "5px",
+      }}
+    >
+      <h2
+        className="text-center"
+        style={{
+          color: "#095B59",
+          textAlign: "center",
+          margin: "1.3em",
+          fontFamily: "Roboto, sans-serif",
+          fontWeight: "lighter",
+          alignContent: "space-around",
+        }}
+      >
+        locvent - admin{" "}
+      </h2>
+      <div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-around",
+            alignItems: "center",
+          }}
+        >
+          <FormGroup>
+            <Col sm={10} md={12}>
+              <Input
+                style={{
+                  color: "#095B59",
+                  fontSize: "1.5em",
+                  height: "1em",
+                  border: "2px solid #095B59",
+                }}
+                type="text"
+                name="userName"
+                id="userName"
+                placeholder="lg"
+                bsSize="lg"
+                color="secondary"
+              />
+            </Col>
+          </FormGroup>
+          <FormGroup>
+            <Col sm={10} md={12}>
+              <Input
+                style={{
+                  color: "#095B59",
+                  fontSize: "1.5em",
+                  height: "1em",
+                  border: "2px solid #095B59",
+                }}
+                type="password"
+                name="password"
+                id="password"
+                placeholder="password"
+                placeholder="lg"
+                bsSize="lg"
+              />
+            </Col>
+          </FormGroup>
+          <div>
+            <Col sm={10} md={12}>
+              <Button
+                style={{
+                  width: "10em",
+                  height: "4em",
+                }}
+                size="bg"
+                color="success"
+                block
+                outline
+              >
+                login
+              </Button>
+            </Col>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
